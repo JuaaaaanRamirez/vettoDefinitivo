dc0c6d6fed5d41b037f591b49af1ac59b9908937
JulioYap <a20203861@pucp.edu.pe>
2023-02-17 15:58:59 -0500

    - Customer: WishList - Sale->Status (char->String^)

--- SalesModel/Customer.h
+++ SalesModel/Customer.h
@@ -6,11 +6,18 @@
 #pragma once
 #include "Person.h"
 using namespace System;
+using namespace System::Collections::Generic;
+
 namespace SalesModel {
     [Serializable]
     public ref class Customer : public Person {
     public:
         property int CustomerPoints;
         property String^ Address;
+        property List<Product^>^ WishList;
+
+        Customer() {
+            WishList = gcnew List<Product^>();
+        }
     };
 }
